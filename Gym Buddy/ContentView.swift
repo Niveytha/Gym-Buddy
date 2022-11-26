8318ec90758ee3f9c741249936fa03bc09953a80
Niveytha Waran <niveytha@gmail.com>
2022-11-26 19:15:02 +0800

    Initial Commit

--- Gym Buddy/ContentView.swift
+++ Gym Buddy/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+//        VStack {
+//            Image(systemName: "figure.run.circle.fill")
+//                .imageScale(.large)
+//                .foregroundColor(.accentColor)
+//                .font(.system(size: 60))
+//            Spacer()
+//                .frame(height: 25)
+//            Text("THIS IS MY GYM BUDDY APP")
+//                .font(.system(size: 36, weight: .black))
+//                .multilineTextAlignment(.center)
+//        }
+//        .padding()
+//        .background(Color.black)
+        ZStack {
+          Color.accentColor
+            .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image(systemName: "figure.run.circle.fill")
+                    .imageScale(.large)
+                    .font(.system(size: 60))
+                Spacer()
+                    .frame(height: 25)
+                Text("THIS IS NIVEYTHA'S GYM BUDDY APP")
+                    .font(.system(size: 36, weight: .black))
+                    .multilineTextAlignment(.center)
+//                    .frame(width: 400)
+            }
         }
-        .padding()
     }
 }
 
