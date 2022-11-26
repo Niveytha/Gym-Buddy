fc1e954f7389739da4886264279168aa72ef61f6
Niveytha Waran <niveytha@gmail.com>
2022-11-26 18:36:57 +0800

    Initial Commit

---
+++ Gym Buddy/Gym_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gym_BuddyApp.swift
+//  Gym Buddy
+//
+//  Created by Niveytha Waran on 26/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Gym_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
